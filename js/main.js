e06dc7f930fb0edca33c885261315fdc942d8106
dmitriy34555 <73700973+dmitriy34555@users.noreply.github.com>
2020-10-30 17:02:05 +0300

    алмалык67кв

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $(".slider").slick({
+    arrows: false,
+    fade: true,
+    autoplay: 3000,
+    dots: true,
+  });
+  $(".header-btn").on("click", function () {
+    $(".menu").addClass("active");
+  });
+  $(".close-btn").on("click", function () {
+    $(".menu").removeClass("active");
+  });
+});
